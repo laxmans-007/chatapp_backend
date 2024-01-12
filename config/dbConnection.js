4b1e18af646e8b1dc21f2cf8763b930e41ecbc61
Laxman Sakure <laxmansakure007@gmail.com>
2024-01-12 22:17:40 +0530

    db options removed

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const dbConnection  = () => {
     const connectionParams = {useNewUrlParser: true, useUnifiedTopology: true};
 
-    mongoose.connect(process.env.MONGODB_URI2, connectionParams);
+    mongoose.connect(process.env.MONGODB_URI2);
 
     mongoose.connection.on("connected", ()=> {
         global.log.error(`Mongodb Connected Successfully.`);
