102b4a156fa54db4a2370a75e83f187940d5b3c1
Laxman Sakure <laxmansakure007@gmail.com>
2024-01-13 16:57:09 +0530

    offline msg change

--- libraries/socketHandler.js
+++ libraries/socketHandler.js
@@ -42,8 +42,10 @@ export default class SocketHandler {
     }
     let messageData = {
       ...this.message,
-      mobileNo: this.user.mobile || "NA"
     };
+    if(!this.isOldMsg) {
+      messageData.mobileNo = this.user.mobile || "NA"
+    }
 
     if (recipents.status == "online" && recipents.socketID) {
       if (global.requestEncryptFlag) {
